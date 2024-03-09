d36f97afabfced338ef33eb36065dd243c346821
DArzoEsp <davidarzola80@gmail.com>
2024-03-09 15:46:35 -0800

    Finished creating map

--- app/index.js
+++ app/index.js
@@ -11,6 +11,10 @@ c.fillRect(0, 0, canvas.width, canvas.height);
 const image = new Image()
 image.src = '../img/Pokemon_map.png'
 
-image.onload = () => {              // used to wait until the image loads to draw image
-    c.drawImage(image, -700, -600);       // requires three arguments to use draw image
+const playerImage = new Image();
+playerImage.src = '../img/ACharRight.png'
+
+image.onload = () => {                // requires three arguments to use draw image
+    c.drawImage(image, -700, -600);         // used to wait until the image loads to draw image
+    c.drawImage(playerImage, 630, 240);
 }
